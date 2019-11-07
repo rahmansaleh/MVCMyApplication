ff27700a5b537a06a2838a02f0040798ea86fc1a
muhammad rahman <rahman9600@gmail.com>
2019-11-07 13:41:50 +0700

    initial commit

---
+++ MVCMyApplication/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMyApplication.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public String Index()
+        {
+            return "hello this is a text from Dashboard Controller";
+        }
+
+        public int showNum()
+        {
+            return 2019;
+        }
+    }
+}
